905e631c433bcc0e2947b5f53a7a9bec776d1700
rahul4019 <rahulkashyap4019@gmail.com>
2023-10-14 06:46:49 +0530

    Configure DB and routes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const studentRoutes = require('./src/students/routes')
+
 const app = express()
 const port = 3000
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api/v1/students', studentRoutes)
 
 app.listen(port, () => {
     console.log(`app listining on port ${port}`)
