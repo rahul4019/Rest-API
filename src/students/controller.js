fdb46422c8aa4bcc0e344431dca204e46693df1e
rahul4019 <rahulkashyap4019@gmail.com>
2023-10-15 19:30:59 +0530

    Add route and controller to get student by id

--- src/students/controller.js
+++ src/students/controller.js
@@ -1,13 +1,23 @@
-const pool = require('../../db')
-const queries = require('../students/queries')
+const pool = require('../../db');
+const queries = require('../students/queries');
 
 const getStudents = (req, res) => {
-    pool.query( queries.getStudents, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows)
-    })
-}
+  pool.query(queries.getStudents, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+
+const getStudentById = (req, res) => {
+  const id = parseInt(req.params.id);
+  // passing id from params to getStudentById query
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
 
 module.exports = {
-    getStudents
-}
+  getStudents,
+  getStudentById,
+};
